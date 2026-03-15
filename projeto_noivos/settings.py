d843e6f31300b45627e7c6a2f4f0a52113c8d490
Piter <piter.augusto1206@gmail.com>
2026-03-15 09:29:16 -0300

    update settings for render

--- projeto_noivos/settings.py
+++ projeto_noivos/settings.py
@@ -123,6 +123,9 @@ STATICFILES_DIRS = [
 import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG") == "True"
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
